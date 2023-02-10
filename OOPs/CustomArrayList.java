9489ae51a3f53293cb2b0ff24dff9eb05de43d0d
jitmanew-tyagi <jitmanew@gmail.com>
2023-02-10 21:05:46 +0530

    ArrayList code

--- OOPs/CustomArrayList.java
+++ OOPs/CustomArrayList.java
@@ -1,7 +1,7 @@
 public class CustomArrayList {
     int cap;
     int[] arr;
-    int nel; //next element location;
+    int nel; // next element location;
 
     CustomArrayList() {
         cap = 10;
@@ -9,16 +9,16 @@ public class CustomArrayList {
         nel = 0;
     }
 
-    public void add(int ele) {
+    public void add(int ele) { //O(1)
         if(nel == cap) grow();
         this.arr[this.nel ++] = ele;
     }
 
-    public int size() {
+    public int size() { // O(1)
         return this.nel;
     }
 
-    public void remove() {
+    public void remove() { // O(1)
         if(nel == 0) {
             System.out.println("No elements to remove");
             return;
@@ -26,23 +26,28 @@ public class CustomArrayList {
         this.nel --;
     }
 
-    public void remove(int idx) {
-
+    public void remove(int idx) { //O(n)
+        if(idx < 0 || idx >= this.size()){
+            System.out.println("idx out of bounds");
+            return;
+        }
+        for(int i = idx; i < this.size() - 1; i ++) arr[i] = arr[i + 1];
+        this.nel --;
     }
 
-    private void grow() {
+    private void grow() { // O(n)
         System.out.println("GROW CALLED");
         //double the capacity
         cap *= 2;
         // make a new array
         int[] temp = new int[cap];
         // copy old stuff
-        for(int i = 0; i < arr.length; i ++) temp[i] = arr[i];
+        for(int i = 0; i < arr.length; i ++) temp[i] = arr[i];  //O(n)
         // arr now points to the new array
         arr = temp;
     }
 
-    public String toString() {
+    public String toString() { O(n)
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for(int i = 0; i < this.size(); i ++)sb.append(arr[i]).append(", ");
@@ -50,5 +55,4 @@ public class CustomArrayList {
         sb.append("]");
         return sb.toString();
     }
-
 }
---
+++ OOPs/CustomStack.java
@@ -0,0 +1,28 @@
+class CustomStack {
+
+
+    public int pop() {
+
+    }  
+
+    public void push(int ele) {
+
+    } 
+
+    public int peek() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
